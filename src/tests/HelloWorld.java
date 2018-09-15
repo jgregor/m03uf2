b74ffe842e731eec3aff51a1102e507034cb1eeb
git <julian@192.168.1.40>
2018-09-15 08:30:46 +0200

    oh, world!

--- src/tests/HelloWorld.java
+++ src/tests/HelloWorld.java
@@ -12,6 +12,6 @@ package tests;
 public class HelloWorld {
     
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, oh, world!");
     }
 }
